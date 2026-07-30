df2ac60b2ac42db29c038c291cf2f190ca2b41e1
sakshampatil142 <sakshampatil142@gmail.com>
2026-07-30 20:59:00 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
-import axios from "axios";
-
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export const getProducts = (params = {}) => api.get("/products", { params }).then((r) => r.data);
